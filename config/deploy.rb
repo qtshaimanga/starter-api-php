22abdc5a3ca0e6dd09ef1dec51789badb1a540e4
quentin.tshaimanga <q.tshaimanga@gmail.com>
2016-09-13 16:18:40 +0200

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'API starter'
+set :repo_url, 'https://github.com/qtshaimanga/starter-api-php.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

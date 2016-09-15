41c4284c710055d32707137e56b6fecc5d6cbcb9
quentin.tshaimanga <q.tshaimanga@gmail.com>
2016-09-15 14:54:35 +0200

    cap integration 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,23 +30,21 @@ set :ssh_options, {:forward_agent => true, :keys => ['~/.ssh/id_rsa.pub']}
 set :linked_files, fetch(:linked_files, []).push('bdd/interaction.sqlite')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('tmp/sessions', 'uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('tmp/sessions', 'uploads', 'var/cache')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
 namespace :deploy do
 
-  after :updated, :composer do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  after :updated, :silex_update do
+    #invoke "silex:composer"
+    #invoke "silex:premissions"
   end
+
+  #after :finished, "silex:restart_apache"
 
 end
---
+++ lib/capistrano/tasks/silex.rake
@@ -0,0 +1,27 @@
+namespace :silex do
+  desc 'Installation des dépendances liées à composer'
+  task :composer do
+    on roles(:web) do
+      within release_path do
+        execute :composer, :install
+      end
+    end
+  end
+
+  desc 'update les permissions bdd, cache et uploads folders'
+  task :premissions do
+    on roles(:web) do
+      within release_path do
+        execute :chmod, '777', '-R', 'uploads'
+      end
+    end
+  end
+
+  desc 'Restart Apache'
+  task :restart_apache do
+    on roles(:web) do
+      sudo :service, 'apache2', :restart
+    end
+  end
+
+end
